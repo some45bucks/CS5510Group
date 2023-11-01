9257349bbde23907ac30a7aed4df210c3ec73b02
Kytech <Kytech@users.noreply.github.com>
2023-10-31 23:34:24 -0600

    Update evaluation loop to support any batch size.

--- Question8/main.py
+++ Question8/main.py
@@ -9,6 +9,8 @@ device_type = 'cuda' if torch.cuda.is_available() else 'cpu'
 device = torch.device(device_type)
 print(f'Using device: {device_type}')
 
+NUM_IMAGES = 100
+
 # Pre-process the images to match the input size of torchvision's AlexNet, using the same pre-processing as in the original paper
 data_transform = Compose([
     Resize(256),
@@ -19,30 +21,38 @@ data_transform = Compose([
 
 print('Loading ImageNet Dataset (this may take a while)...')
 validation_set = ImageNet(root='./dataset', split='val', transform=data_transform)
-print('Finished loading ImageNet Dataset')
+print('Finished loading ImageNet Dataset\n')
 
 print('Loading AlexNet model with pre-trained weights...')
 model = alexnet(weights='IMAGENET1K_V1').eval().to(device)
-print('Finished loading AlexNet model')
+print('Finished loading AlexNet model\n')
 
-# I'm going to assume that most GPUs that will run this should have enough memory to load all 100 images in a batch for inference
-# So that I can simplify the code and not have to worry about computing metrics across multiple batches
-val_loader = DataLoader(validation_set, batch_size=100, sampler=RandomSampler(validation_set, num_samples=100))
+val_loader = DataLoader(validation_set, batch_size=64, sampler=RandomSampler(validation_set, num_samples=NUM_IMAGES))
 
 print('Performing classification on ImageNet validation set...')
+top_1_correct_count = 0
+top_5_correct_count = 0
+batch_num = 0
 with torch.no_grad():
     for images, labels in val_loader:
+        batch_num += 1
         images, labels = images.to(device), labels.to(device)
 
         outputs = model(images)
         _, top_5_predictions = torch.topk(outputs, k=5, dim=1)
         top_1_predictions = top_5_predictions[:, 0]
 
-        top_1_accuracy = torch.sum(top_1_predictions == labels).item() / labels.size(0) * 100
-        top_5_accuracy = torch.sum(torch.any(top_5_predictions == labels.unsqueeze(dim=1), dim=1)).item() / labels.size(0) * 100
+        top_1_correct_count += torch.sum(top_1_predictions == labels).item()
+        top_5_correct_count += torch.sum(torch.any(top_5_predictions == labels.unsqueeze(dim=1), dim=1)).item()
+        
+        top_1_batch_accuracy = torch.sum(top_1_predictions == labels).item() / labels.size(0)
+        top_5_batch_accuracy = torch.sum(torch.any(top_5_predictions == labels.unsqueeze(dim=1), dim=1)).item() / labels.size(0)
+        print(f'Batch {batch_num}: Top 1 Accuracy: {top_1_batch_accuracy * 100:.2f}%, Top 5 Accuracy: {top_5_batch_accuracy * 100:.2f}%')
 
-        print(f'Top-1 Accuracy: {top_1_accuracy:.2f}%')
-        print(f'Top-5 Accuracy: {top_5_accuracy:.2f}%')
-        break # Only classify one batch of 100 images
+print('Finished performing classification\n')
 
-print('Finished performing classification')
+print('Results:')
+top_1_accuracy = top_1_correct_count / val_loader.sampler.num_samples
+top_5_accuracy = top_5_correct_count / val_loader.sampler.num_samples
+print(f'Top-1 Accuracy: {top_1_accuracy * 100:.2f}%')
+print(f'Top-5 Accuracy: {top_5_accuracy * 100:.2f}%')
